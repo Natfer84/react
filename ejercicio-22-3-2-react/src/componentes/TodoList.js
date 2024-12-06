190b7f4adec8a9d91b9cab861b8bec92cb7c26c6
Natfer84 <natita2804@hotmail.com>
2024-12-06 20:34:20 +0100

    tareas

--- ejercicio-22-3-2-react/src/componentes/TodoList.js
+++ ejercicio-22-3-2-react/src/componentes/TodoList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "../hojas-de-estilo/todoList.css";
 const TodoList = () => {
   /* useState para el input */
   const [tarea, setTarea] = useState("");
@@ -7,57 +7,69 @@ const TodoList = () => {
   const [arrayTareas, setArrayTarea] = useState([]);
   //const [borrarArray, setBorrarArray] = useState(arrayTareas);
 
- 
-
   /*funcion para tratar los datos del input*/
   const handleChange = (event) => {
     setTarea(event.target.value);
   };
 
   /*funcion para meter tarea en array*/
+  //id: Date.now() genera un id único que le otorga a cada elemento del array
   const tareas = () => {
-    if (tarea.trim() !== "") setArrayTarea([...arrayTareas, {id: Date.now(), tarea }]);
+    if (tarea.trim() !== "")
+      setArrayTarea([...arrayTareas, { id: Date.now(), tarea }]);
     setTarea("");
   };
-  console.log(arrayTareas);
-
-  
+  //console.log(arrayTareas);
 
   return (
-    <div>
-      <input type="text" value={tarea} onChange={handleChange} />
+    
+    <div className="containerTasks">
+     
+      <input
+        type="text"
+        value={tarea}
+        onChange={handleChange}
+        className="containerTasks__Input"
+      />
 
-      <ul>
+      <ul className="containerTasks__Ul">
         {arrayTareas.map((posicion) => (
-          <li key={posicion.id}>
+          <li key={posicion.id} className="containerTasks__Ul__Li">
             {posicion.tarea}{" "}
-            <button
+            <div
+              className="containerTasks__ButtonEliminar"
               onClick={() => {
-                setArrayTarea(arrayTareas.filter(tarea => tarea.id !== posicion.id ))
+                setArrayTarea(
+                  arrayTareas.filter((tarea) => tarea.id !== posicion.id)
+                );
               }}
             >
               Eliminar
-            </button>
+            </div>
           </li>
         ))}
       </ul>
 
-      <button
+        <div className="container_button">
+      <div
+        className="containerTasks__ButtonAñadirTarea"
         onClick={() => {
           tareas();
         }}
       >
         Añadir tarea
-      </button>
+      </div>
 
-      <button
+      <div
+        className="containerTasks__ButtonEliminarUltTarea"
         onClick={() => {
-          setArrayTarea(arrayTareas.slice(0,-1));
+          setArrayTarea(arrayTareas.slice(0, -1));
         }}
       >
         Eliminar última tarea
-      </button>
+      </div>
 
+      </div>
     </div>
   );
 };
